de669756b060b5c991a6cb5f7061bc5a510c8f55
nurfandi31 <fandinur241@gmail.com>
2025-08-20 15:16:34 +0700

    karyawan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,26 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Level;
+use App\Models\Presensi;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $title = 'Karyawan';
-        $users = User::all();
-        return view('app.admin.karyawan.index', compact('title', 'users'));
+        if ($request->ajax()) {
+            $data = User::with('level')->get();
+
+            return DataTables::of($data)->make(true);
+        }
+
+        return view('app.admin.karyawan.index', ['title' => 'Karyawan']);
     }
+
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        $title = 'Tambah Karyawan';
+        $levels = Level::all();
+
+        return view('app.admin.karyawan.create', compact('title', 'levels'));
     }
 
     /**
@@ -30,13 +45,65 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->only([
+            "level_id",
+            "nama",
+            "nik",
+            "tanggal_lahir",
+            "tanggal_masuk",
+            "gaji",
+            "alamat",
+            "telpon",
+            "jenis_kelamin",
+            "username",
+            "password"
+        ]);
+
+        $rules = [
+            'level_id'           => 'required',
+            'nama'               => 'required',
+            'nik'                => 'required',
+            'tanggal_lahir'      => 'required',
+            'tanggal_masuk'      => 'required',
+            'gaji'               => 'required',
+            'alamat'             => 'required',
+            'telpon'             => 'nullable',
+            'jenis_kelamin'      => 'required',
+            'username'           => 'required',
+            'password'           => 'required',
+        ];
+
+        $gaji = floatval(str_replace(',', '', str_replace('.00', '', $request->gaji)));
+
+        $validate = Validator::make($data, $rules);
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $karyawan = User::create([
+            'level_id' => $request->level_id,
+            'nik' => $request->nik,
+            'nama' => $request->nama,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'tanggal_masuk' => $request->tanggal_masuk,
+            'gaji' => $gaji,
+            'alamat' => $request->alamat,
+            'telpon' => $request->telpon,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'id_sidik_jari' => 0,
+            'status' => 'aktif',
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json([
+            'success' => true,
+            'msg' => 'Karyawan berhasil ditambahkan!',
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show(User $karyawan)
     {
         //
     }
@@ -44,24 +111,91 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user)
+    public function edit(User $karyawan)
     {
-        //
+        $title = 'Edit Karyawan';
+        $levels = Level::all();
+
+        return view('app.admin.karyawan.edit', compact('title', 'karyawan', 'levels'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, User $karyawan)
     {
-        //
+        $data = $request->only([
+            "level_id",
+            "nama",
+            "nik",
+            "tanggal_lahir",
+            "tanggal_masuk",
+            "gaji",
+            "alamat",
+            "telpon",
+            "jenis_kelamin",
+            "username",
+            "password"
+        ]);
+
+        $rules = [
+            'level_id'           => 'required',
+            'nama'               => 'required',
+            'nik'                => 'required',
+            'tanggal_lahir'      => 'required',
+            'tanggal_masuk'      => 'required',
+            'gaji'               => 'required',
+            'alamat'             => 'required',
+            'telpon'             => 'nullable',
+            'jenis_kelamin'      => 'required',
+            'username'           => 'required',
+        ];
+
+        $gaji = floatval(str_replace(',', '', str_replace('.00', '', $request->gaji)));
+
+        $validate = Validator::make($data, $rules);
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $karyawan->update([
+            'level_id' => $request->level_id,
+            'nik' => $request->nik,
+            'nama' => $request->nama,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'tanggal_masuk' => $request->tanggal_masuk,
+            'gaji' => $gaji,
+            'alamat' => $request->alamat,
+            'telpon' => $request->telpon,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'id_sidik_jari' => 0,
+            'status' => 'aktif',
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json([
+            'success' => true,
+            'msg' => 'Karyawan berhasil di Update!',
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+
+    public function destroy(User $karyawan)
     {
-        //
+        if (Presensi::where('user_id', $karyawan->id)->exists()) {
+            return response()->json([
+                'status'  => 'error',
+                'message' => 'Data tidak bisa dihapus karena sudah dipakai di data presensi.'
+            ], 400);
+        }
+
+        $karyawan->delete();
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Karyawan berhasil dihapus.'
+        ]);
     }
 }
---
+++ resources/views/app/Admin/karyawan/create.blade.php
@@ -0,0 +1,187 @@
+@extends('app.layouts.app')
+@section('content')
+    <form action="/app/karyawan" method="post" id="FromKaryawan">
+        @csrf
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title mb-0">
+                    <p class="card-subtitle">Tambah Karyawan Baru</p>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="mb-6">
+                            <label for="nik" class="form-label">Nik</label>
+                            <input type="text" maxlength="16" class="form-control" id="nik" name="nik"
+                                placeholder="masukkan nik karyawan">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mb-6">
+                            <label for="nama" class="form-label">Nama Lengkap</label>
+                            <input type="text" class="form-control" id="nama" name="nama"
+                                placeholder="masukkan nama karyawan">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mb-6">
+                            <label for="level_id" class="form-label">Level Hak Akses</label>
+                            <select id="level_id" name="level_id" class="select2 form-select form-select-lg"
+                                data-allow-clear="true">
+                                <option value="">-- Pilih Bahan --</option>
+                                @foreach ($levels as $lev)
+                                    <option value="{{ $lev->id }}">{{ $lev->nama }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <label class="form-label" for="tanggal_lahir">Tanggal Lahir</label>
+                        <input type="text" id="tanggal_lahir" name="tanggal_lahir" class="form-control dob-picker"
+                            placeholder="YYYY-MM-DD" value="{{ date('Y-m-d') }}" />
+                    </div>
+                    <div class="col-4">
+                        <div class="mb-6">
+                            <label class="form-label d-block">Jenis Kelamin</label>
+                            <div class="btn-group w-100" role="group">
+                                <input type="radio" class="btn-check" name="jenis_kelamin" id="laki" value="L"
+                                    autocomplete="off">
+                                <label class="btn btn-outline-secondary w-50" for="laki">Laki-laki</label>
+                                <input type="radio" class="btn-check" name="jenis_kelamin" id="perempuan" value="P"
+                                    autocomplete="off">
+                                <label class="btn btn-outline-secondary w-50" for="perempuan">Perempuan</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mb-6">
+                            <label for="telpon" class="form-label">No Telepon</label>
+                            <input type="text" class="form-control" id="telpon" name="telpon"
+                                placeholder="masukkan no telepon">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label class="form-label" for="tanggal_masuk">Tanggal Masuk</label>
+                        <input type="text" id="tanggal_masuk" name="tanggal_masuk" class="form-control dob-picker"
+                            placeholder="YYYY-MM-DD" value="{{ date('Y-m-d') }}" />
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-6">
+                            <label for="gaji" class="form-label">Satuan Gaji</label>
+                            <input type="text" class="form-control" id="gaji" name="gaji"
+                                placeholder="masukkan satuan gaji">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-412">
+                        <div class="mb-6">
+                            <label for="alamat" class="form-label">Alamat Lengkap</label>
+                            <textarea class="form-control" id="alamat" name="alamat" rows="1" placeholder="Alamat Lengkap"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-6">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="username" name="username"
+                                placeholder="masukkan username">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-6">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="text" class="form-control" id="password" name="password"
+                                placeholder="masukkan password">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-0 d-flex justify-content-between align-items-center">
+                    <a href="/app/karyawan" class="btn btn-outline-secondary">
+                        <i class="icon-base bx bx-left-arrow-alt me-1"></i>
+                        <span class="align-middle">Kembali</span>
+                    </a>
+                    <div d-flex>
+                        <button type="button" id="simpanKaryawan" class="btn btn-primary ms-2">
+                            <i class="icon-base bx bx-cloud-upload me-1"></i>
+                            <span class="align-middle">Simpan</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
+@section('script')
+    <script>
+        $(document).on('change', '.select2', function() {
+            const selectedOption = $(this).find('option:selected');
+            const bahanData = JSON.parse(selectedOption.val());
+
+            var inputGroup = $(this).closest('.bahan').next('.jumlah');
+            inputGroup.find('span.input-group-text').text(bahanData.satuan);
+        });
+
+        $(".dob-picker").flatpickr({
+            monthSelectorType: "static"
+        })
+
+        $("#gaji").on("input", function() {
+            let value = $(this).val().replace(/\D/g, "");
+            $(this).val(new Intl.NumberFormat("id-ID").format(value));
+        });
+
+        $(document).on('click', '#simpanKaryawan', function(e) {
+            e.preventDefault();
+            $('small').html('');
+            $('.is-invalid').removeClass('is-invalid');
+
+            var form = $('#FromKaryawan');
+            var actionUrl = form.attr('action');
+
+            $.ajax({
+                type: 'POST',
+                url: actionUrl,
+                data: form.serialize(),
+                success: function(result) {
+                    if (result.success) {
+                        Swal.fire({
+                            title: result.msg,
+                            text: "Tambahkan Register Karyawan Baru?",
+                            icon: "success",
+                            showDenyButton: true,
+                            confirmButtonText: "Tambahkan",
+                            denyButtonText: `Tidak`
+                        }).then((res) => {
+                            if (res.isConfirmed) {
+                                window.location.reload();
+                            } else {
+                                window.location.href = '/app/karyawan';
+                            }
+                        });
+                    }
+                },
+                error: function(result) {
+                    const response = result.responseJSON;
+                    Swal.fire('Error', 'Cek kembali input yang anda masukkan', 'error');
+                    if (response && typeof response === 'object') {
+                        $.each(response, function(key, message) {
+                            $('#' + key).addClass('is-invalid');
+                            $('#msg_' + key).html(message[0]);
+                        });
+                    }
+                }
+            });
+        });
+    </script>
+@endsection
