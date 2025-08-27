733d8b63a9c7b93c71b0fb6540d5a72859e8ae86
arafi118 <arp46301@gmail.com>
2025-08-27 10:42:50 +0700

    Rekening

---
+++ app/Models/AkunLevel3.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AkunLevel3 extends Model
+{
+    use HasFactory;
+
+    protected $table = 'akun_level3s';
+}
