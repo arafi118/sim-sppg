6df8f3d5d543b7b2c42b9c52a54b154eeb9b8c50
nurfandi31 <fandinur241@gmail.com>
2025-08-27 08:53:54 +0700

    pelaporan pks

--- resources/views/app/pelaporan/layout/base.blade.php
+++ resources/views/app/pelaporan/layout/base.blade.php
@@ -1,34 +1,94 @@
- <meta charset="UTF-8">
- <title>{{ $title }}</title>
+<!DOCTYPE html>
+<html lang="id">
 
- <style>
-     * {
-         font-family: 'Arial', sans-serif;
-         font-size: 12px;
-         line-height: 1.5;
-     }
+<head>
+    <meta charset="UTF-8">
+    <title>{{ $title ?? 'Dokumen' }}</title>
 
-     .judul {
-         text-align: center;
-         font-weight: bold;
-         font-size: 14px;
-         text-transform: uppercase;
-         margin-bottom: 30px;
-     }
+    <style>
+        * {
+            font-family: 'Arial', sans-serif;
+            font-size: 12px;
+            line-height: 1.5;
+        }
 
-     .subjudul {
-         text-align: left;
-         margin-bottom: 20px;
-     }
+        .judul {
+            text-align: center;
+            font-weight: bold;
+            font-size: 14px;
+            text-transform: uppercase;
+            margin-bottom: 30px;
+        }
 
-     table {
-         border-collapse: collapse;
-         width: 100%;
-     }
+        .subjudul {
+            text-align: left;
+            margin-bottom: 20px;
+        }
 
-     .tb-border th,
-     .tb-border td {
-         border: 1px solid #000;
-         padding: 4px;
-     }
- </style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        .tb-border th,
+        .tb-border td {
+            border: 1px solid #000;
+            padding: 4px;
+        }
+
+        .rata-kanan {
+            text-align: right;
+        }
+
+        .rata-kiri {
+            text-align: left;
+        }
+
+        .rata-tengah {
+            text-align: center;
+        }
+
+        .ratakanankiri {
+            text-align: justify;
+            text-justify: inter-word;
+        }
+
+        .break {
+            page-break-after: always;
+        }
+
+        ol.romawi,
+        ol.angka,
+        ol.huruf-kecil,
+        ol.huruf-besar {
+            margin: 0;
+            padding-left: 20px;
+        }
+
+        ol.romawi {
+            list-style-type: upper-roman;
+            padding-top: 5px;
+        }
+
+        ol.angka {
+            list-style-type: decimal;
+            padding-top: 5px;
+        }
+
+        ol.huruf-kecil {
+            list-style-type: lower-alpha;
+            padding-top: 5px;
+        }
+
+        ol.huruf-besar {
+            list-style-type: upper-alpha;
+            padding-top: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
