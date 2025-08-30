ea21105a59cbd7617a6ebd56b924f5552682d211
nurfandi31 <fandinur241@gmail.com>
2025-08-30 09:30:36 +0700

    update

--- database/migrations/2025_08_19_023941_create_kelompok_pemanfaats_table.php
+++ database/migrations/2025_08_19_023941_create_kelompok_pemanfaats_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('kelompok_pemanfaats', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('nama');
+            $table->string('kode');
             $table->timestamps();
         });
     }
