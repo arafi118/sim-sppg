f40a0bebb47d2a3eda55e017edf938e35d43f718
arafi118 <arp46301@gmail.com>
2025-09-09 09:48:30 +0700

    card

--- resources/views/app/transaksi/index.blade.php
+++ resources/views/app/transaksi/index.blade.php
@@ -1,46 +1,47 @@
 @extends('app.layouts.app')
 @section('content')
-    <div class="container-fluid" id="container-wrapper">
-        <form action="/transaksi/store" method="post" id="FormTransaksi">
-            @csrf
-            <div class="row">
-                <div class="col-md-6">
-                    <label for="tgl_transaksi_display">Tanggal Transaksi</label>
-                    <input type="text" class="form-control" id="tgl_transaksi_display" placeholder="Pilih tanggal">
-                    <input type="hidden" name="tanggal_transaksi" id="tgl_transaksi" value="{{ date('Y-m-d') }}">
+    <div class="card">
+        <div class="card-body">
+            <form action="/transaksi/store" method="post" id="FormTransaksi">
+                @csrf
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="tgl_transaksi_display">Tanggal Transaksi</label>
+                        <input type="text" class="form-control" id="tgl_transaksi_display" placeholder="Pilih tanggal">
+                        <input type="hidden" name="tanggal_transaksi" id="tgl_transaksi" value="{{ date('Y-m-d') }}">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="bahan_pangan_id">Nama Bahan</label>
+                        <select class="form-control select2" name="bahan_pangan_id" id="bahan_pangan_id" required>
+                            <option value="">-- Pilih Barang --</option>
+                            @foreach ($bahanpangan as $b)
+                                <option value="{{ $b->id }}" data-harga="{{ $b->harga_jual }}">{{ $b->nama }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <label for="bahan_pangan_id">Nama Bahan</label>
-                    <select class="form-control select2" name="bahan_pangan_id" id="bahan_pangan_id" required>
-                        <option value="">-- Pilih Barang --</option>
-                        @foreach ($bahanpangan as $b)
-                            <option value="{{ $b->id }}" data-harga="{{ $b->harga_jual }}">{{ $b->nama }}
-                            </option>
-                        @endforeach
-                    </select>
+                <div class="row mt-3">
+                    <div class="col-md-6">
+                        <label for="jumlah">Jumlah</label>
+                        <input type="number" class="form-control" name="jumlah" id="jumlah" min="1" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="harga_jual">Harga Jual</label>
+                        <input type="number" class="form-control" id="harga_jual" readonly>
+                    </div>
                 </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-md-6">
-                    <label for="jumlah">Jumlah</label>
-                    <input type="number" class="form-control" name="jumlah" id="jumlah" min="1" required>
+                <div class="row mt-3">
+                    <div class="col-md-12">
+                        <label for="nominal">Total Harga</label>
+                        <input type="number" class="form-control" name="jumlah_total" id="nominal" readonly>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <label for="harga_jual">Harga Jual</label>
-                    <input type="number" class="form-control" id="harga_jual" readonly>
+                <div class="mt-3 text-end">
+                    <button class="btn btn-primary" type="submit">Simpan Transaksi</button>
                 </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-md-12">
-                    <label for="nominal">Total Harga</label>
-                    <input type="number" class="form-control" name="jumlah_total" id="nominal" readonly>
-                </div>
-            </div>
-            <div class="mt-3 text-end">
-                <button class="btn btn-primary" type="submit">Simpan Transaksi</button>
-            </div>
-        </form>
-
+            </form>
+        </div>
     </div>
 @endsection
 
