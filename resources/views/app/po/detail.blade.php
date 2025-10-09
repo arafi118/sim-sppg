5ddf984d95ea17374e134e05572a254dc89cb60f
arafi118 <arp46301@gmail.com>
2025-10-09 08:57:32 +0700

    Perbaikan perhitungan total PO

--- resources/views/app/po/detail.blade.php
+++ resources/views/app/po/detail.blade.php
@@ -1,29 +1,37 @@
-<table class="table">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Nama Bahan</th>
-            <th>Harga</th>
-            <th>Jumlah</th>
-            <th>Total</th>
-        </tr>
-    </thead>
-
-    <tbody>
-        @foreach ($po->poDetail as $detail)
+<div class="table-responsive">
+    <table class="table">
+        <thead>
             <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $detail->bahanPangan->nama }}</td>
-                <td class="text-end">{{ number_format($detail->harga_satuan) }}</td>
-                <td>{{ number_format($detail->jumlah, 2) }} ({{ $detail->bahanPangan->satuan }})</td>
-                <td class="text-end">{{ number_format($detail->total_harga) }}</td>
+                <th>#</th>
+                <th>Nama Bahan</th>
+                <th>Harga</th>
+                <th>Jumlah</th>
+                <th>Total</th>
             </tr>
-        @endforeach
-    </tbody>
-    <tfoot>
-        <tr>
-            <th colspan="4">Total</th>
-            <th class="text-end">{{ number_format($po->total_harga) }}</th>
-        </tr>
-    </tfoot>
-</table>
+        </thead>
+
+        <tbody>
+            @php
+                $total = 0;
+            @endphp
+            @foreach ($po->poDetail as $detail)
+                @php
+                    $total += $detail->total_harga;
+                @endphp
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $detail->bahanPangan->nama }}</td>
+                    <td class="text-end">{{ number_format($detail->harga_satuan) }}</td>
+                    <td>{{ number_format($detail->jumlah, 2) }} ({{ $detail->bahanPangan->satuan }})</td>
+                    <td class="text-end">{{ number_format($detail->total_harga) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="4">Total</th>
+                <th class="text-end">{{ number_format($total) }}</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
